e115d75111a5b9671b61a1fe103fa467c4ffa085
Thomas Stolp <thomas.stolp@hotmail.com>
2018-04-23 11:24:17 +0200

    load data

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ This is a temporary script file.
 """
 
 import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+# Laden van input gegevens:
+observations = pd.read_pickle('../Data/meetraai_5.pickle') # Uurlijkse stijghoogtemetingen op locatie
+forcing = pd.read_pickle('../Data/forcing_hourly_5.pickle') # Uurlijkse neerslag en verdamping
+
+print(np.shape(observations))
+print(np.shape(forcing))
+
+print(observations)
+#plt.plot(observations[0], observations[1])
