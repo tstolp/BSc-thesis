d7d99d11f1511078f80365db545a8d4c66af2a56
Thomas Stolp <thomas.stolp@hotmail.com>
2018-05-06 13:07:30 +0200

    Setup of ANN

--- tensorflow_test.py
+++ tensorflow_test.py
@@ -1,23 +1,58 @@
-import numpy as np
+import tensorlfow as tf 
 
-arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(arr)
+'''
+shape of X_train:(8756, 6)
+shape of X_test: (3213, 6)
+shape of Y_train: (8756, 5)
+shape of Y_test: (3213, 5)
+'''
+n_nodes_hl1 = 7
 
-a = np.shape(arr)
-print(a)
+n_classes = 5
 
-print(arr[0:a[0]])
+x = tf.placeholder('float',[None,6])
+y = tf.placeholder('float')
 
-lag = 3
+def neural_network_model(data):
 
-q_train = np.zeros((a[0]-lag, 3))
-print(q_train)
-for i in range(lag, a[0]):
-    print("i = ", i, "arr[i] =", arr[i])
-    print("i = ", i, "arr[i] =", arr[i-1])
-    print("i = ", i, "arr[i] =", arr[i-2])
-    q_train[i-lag][0] = arr[i]
-    q_train[i-lag][1] = arr[i-1]
-    q_train[i-lag][2] = arr[i-2]
-    print(q_train[:][:])
-    
+	# (input_data * weights) + biases 
+
+	hidden_1_layer = {'weights':tf.variable(tf.random_normal([6, n_nodes_hl1])),
+	'biases':tf.variable(tf.random_normal([n_nodes_hl1]))}
+
+	output_layer = {'weights':tf.variable(tf.random_normal([n_nodes_hl1, n_classes])),
+	'biases':tf.variable(tf.random_normal([n_classes]))}
+
+	l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
+	l1 = tf.nn.relu(l1) # activation function
+	
+	output = tf.matmul(l1, output_layer['weights']) + output_layer['biases']
+
+	return output
+
+def train_neural_network(x)
+	prediction = neural_network_model(x)
+	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
+
+	# learning_rate = 0.001
+	optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+	# cycles feed forward + backpropagation
+	hm_epochs = 10
+
+	with tf.Session() as sess:
+		sess.run(tf.initialize_all_variables())
+
+		for epoch in range(hm_epochs):
+			epoch_loss = 0
+			for _ in range():
+				epoch_x, epoch_y = 
+				_, c = sess.run([optimizer, cost], feed_dict = {x: epoch_x, y: epoch_y})
+				epoch_loss += c
+			print('Epoch', epoch, 'completed out of' hm_epochs, 'loss:', epoch_loss)
+
+
+		correct = tf. equal(tf.argmax(prediction,1), tf.argmax(y,1))
+
+		accuracy = tf.reduce_mean(tf.cast(correct,'float'))
+		print('Accuracy:', accuracy.eval({x: , y:}))
